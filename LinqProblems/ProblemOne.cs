63fbdb34270feba82aef34a27646ae192d840c1a
kelvin omoregie <kayo2k14@gmail.com>
2020-03-09 15:10:54 -0500

    init

---
+++ LinqProblems/ProblemOne.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqProblems
+{
+    class ProblemOne
+    {
+        List<string> words;
+        public ProblemOne()
+        {
+            words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+        }
+
+        public class WordList()
+
+            var words = new WordList().GetWord();
+            var
+        
+            foreach (var word in words )
+    }
+  
+}
